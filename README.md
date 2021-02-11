46e09da6fefba987c5f27cfbdefcaedb67c228c0
Mbk15 <mubarakmuhammadjamiu@gmail.com>
2021-02-11 09:07:46 +0100

    Ramadan Update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-1.  Countdown Timer
--    A timer that counts down till next EID-UL_ADHA Celebration (Musilms Sacrificial festival)
+1.  Countdown Timer with background Sahur setup and Pause/play feature 
+-    A timer that counts down till next RAMADAN  (9th Month of Islamic Calendar), where Muslims fast for 29 or 30 days owing to the Lunar config
 -    future date
 -     timer
+-
--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+var name 
+ name = prompt("Enter your name here");
+ alert(` Welcome ${name}`);
+
+
 const  days = document.querySelector("#days")
 const  hours = document.querySelector("#hours")
 const  mins = document.querySelector("#mins")
 const  seconds = document.querySelector("#seconds")
 
-const AdhaDate = "20 Jun 2021";
+const RmdDate = "12 Apr 2021";
 
 countdown= () =>{
-    const EidDate =new Date(AdhaDate);
+    const Hilal =new Date(RmdDate);
     const currentDate = new Date();
-    const totalSec = (EidDate - currentDate)/1000;
+    const totalSec = (Hilal - currentDate)/1000;
     const minutes = Math.floor(totalSec/60)%60;
     const hour = Math.floor(totalSec/3600) % 24;
     const day = Math.floor((totalSec/3600/24));
@@ -20,3 +25,25 @@ countdown= () =>{
 }
 countdown();
 setInterval(countdown, 1000)
+ message= () =>{
+    alert("Allahuma Baligna Ramadan, Dont forget to complete your missed fast")
+    alert(`Check below to see the number of days left, Ramadan Kareem in advance ${name} `)
+ }
+ message();
+// get the video
+ var video =document.getElementById("myVideo");
+
+ //get the button
+ var btn = document.getElementById("myBtn");
+ //pause and play video, and change next button
+
+ function myFunction(){
+     if(video.paused){
+         video.play();
+
+         btn.innerHTML ="Pause"
+     }else{
+         video.pause();
+         btn.innerHTML = "Play"
+     }
+ }
